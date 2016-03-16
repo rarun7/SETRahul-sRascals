9e03660a27e02e24bae3331e6f6c16d3fd29359e
John <jtan8@students.d125.org>
2016-03-16 10:33:33 -0500

    update

--- CodeHandouts/Group1/Deck.java
+++ CodeHandouts/Group1/Deck.java
@@ -1,23 +1,77 @@
-/**
- *  Represents a generic deck of cards.
- */
-
-/*
-
-  Tips:
-
-  - Use an ArrayList<Card> to hold the cards.
-  - Add and remove cards at the end of the list.
-  - Use Collections.shuffle and Collections.sort to shuffle and sort the deck,
-    or write your own methods.  If you write your own, use selection sort
-    to sort and a similar algorithm to shuffle.  Use Math.random().
-  - In the toString method, separate strings for individual cards with "\n".
-
-*/
-
 import java.util.*;
 
+/**
+ * Write a description of class Deck here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
 public class Deck
 {
-  ...
+    ArrayList<Card> bubbles=new ArrayList<Card>();
+    public Deck () //81 cards
+    {
+        
+        for(int i=1; i<82; i++)
+        {
+            Card test=new Card(i-1);
+            bubbles.add(test);
+        }
+    }
+
+    public Deck (int capacity)// creates an empty deck of given capacity
+    {
+        
+        for(int i=1; i<=capacity; i++)
+        {
+            Card test=new Card(i);
+            bubbles.add(test);
+        }
+    }
+
+    public int getNumCards ()
+    {
+        return bubbles.size();
+    }
+
+    public boolean isEmpty ()
+    {
+        if(bubbles.size()==0)
+            return true;
+        else
+            return false;
+    }
+
+        public void add (Card card)
+    {
+        bubbles.add(0,card);
+    }
+
+    public Card takeTop()
+    {
+
+        return bubbles.remove(bubbles.size()-1);
+        
+    }
+
+    public void shuffle ()
+    {
+        Collections.shuffle(bubbles);
+    }
+
+    public void sort()
+    {
+        Collections.sort(bubbles);
+    }
+
+    public String toString()
+    {
+        String s="";
+        for(int i=0; i<bubbles.size(); i++)
+        {
+            s+=""+bubbles.get(i)+"\n";
+        }
+        return s;
+        
+    }
 }
